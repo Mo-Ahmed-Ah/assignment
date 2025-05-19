fbc23fe9a1f9431924fe1e2c907622ccdf568409
Ahmed Mohamed <ahmed0033ahmed.mo@gmail.com>
2025-05-19 07:58:59 +0300

    finsh the assignment one and propelm one in part one in assignment two

---
+++ C#/assignmentNO_01_Ahmed/Program.cs
@@ -0,0 +1,186 @@
+﻿namespace assignmentNO_01_Ahmed
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Proplem1 
+            //int number;
+
+            //Console.Write("Enter the number : ");
+
+            //number = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine(number);
+            #endregion
+
+            #region Proplem2 
+            //int number;
+
+            //Console.Write("Enter the number : ");
+
+            //number = int.Parse(Console.ReadLine());
+
+            //if (number % 3 == 0 && number % 4 == 0)
+            //    Console.WriteLine("yse");
+            //else Console.WriteLine("No");
+            #endregion
+
+            #region Proplem3
+            //    int numberOne , numberTwo ;
+            //    Console.Write("Enter the number one : ");
+            //    numberOne = int.Parse(Console.ReadLine());
+            //    Console.Write("Enter the number two : ");
+            //    numberTwo = int.Parse(Console.ReadLine());
+            //if (numberOne > numberTwo)
+            //    Console.WriteLine($"max = {numberOne}");
+            //else if(numberOne < numberTwo)
+            //    Console.WriteLine($"max = {numberTwo}");
+
+
+            #endregion
+
+            #region Proplem4
+            //int number;
+            //Console.Write("Enter number : ");
+            //int.TryParse(Console.ReadLine(), out number);
+            //if (number < 0) Console.WriteLine("negative");
+            //else Console.WriteLine("positive");
+            #endregion
+
+            #region Proplem5
+            //int numberOne , numberTwo , numberThree;
+            //Console.Write("Enter the number One : ");
+            //int.TryParse(Console.ReadLine(), out numberOne);
+            //Console.Write("Enter the number Two : ");
+            //int.TryParse(Console.ReadLine(), out numberTwo);
+            //Console.Write("Enter the number Three : ");
+            //int.TryParse(Console.ReadLine(), out numberThree);
+
+            //if (numberOne > numberTwo && numberOne > numberThree)
+            //{
+            //    if (numberTwo < numberThree)
+            //    {
+            //        Console.WriteLine($"Max number  = {numberOne} and Min Number = {numberTwo}");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"Max number  = {numberOne} and Min Number = {numberThree}");
+
+            //    }
+            //} 
+            //else if (numberTwo > numberOne && numberTwo >numberThree) 
+            //{
+            //     if(numberOne < numberThree)
+            //    {
+            //        Console.WriteLine($"Max number  = {numberTwo} and Min Number = {numberOne}");
+
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"Max number  = {numberTwo} and Min Number = {numberThree}");
+
+            //    }
+            //}
+            //else
+            //{
+            //    if (numberOne < numberTwo)
+            //    {
+            //        Console.WriteLine($"Max number  = {numberThree} and Min Number = {numberOne}");
+
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"Max number  = {numberThree} and Min Number = {numberTwo}");
+
+            //    }
+            //}
+            #endregion
+
+            #region Proplem6
+            //int number;
+            //Console.Write("Enter the number : ");
+            //int.TryParse(Console.ReadLine(), out number);
+            //if (number % 2 == 0) Console.WriteLine("odd");
+            //else Console.WriteLine("even");
+            #endregion
+
+            #region Proplem7
+            //char characther;
+            //Console.Write("Enter the character : ");
+            //char.TryParse(Console.ReadLine(), out characther);
+            //switch (characther)
+            //{
+            //    case 'a' or  'e' or 'i' or 'o' or 'u' or 'A' or 'E' or 'I' or 'O' or 'U':
+            //        Console.WriteLine("vowel");
+            //        break;
+            //    default:
+            //        Console.WriteLine("consonant");
+            //        break ;
+            //}
+            #endregion
+
+            #region Proplem8
+            //int number;
+            //Console.Write("Enter the number : ");
+            //int.TryParse(Console.ReadLine(), out number);
+            //for(int i = 1; i <= number; i++) 
+            //    Console.WriteLine(i);
+            #endregion
+
+            #region Proplem9
+            //int number;
+            //Console.Write("Enter the number : ");
+            //int.TryParse(Console.ReadLine(), out number);
+            //for (int i = 1; i <= 12; i++)
+            //    Console.WriteLine(number * i);
+
+            #endregion
+
+            #region Proplem10
+            //int number;
+            //Console.Write("Enter the number : ");
+            //int.TryParse(Console.ReadLine(), out number);
+            //for (int i = 1; i <= number; i++)
+            //    if (i % 2 == 0) Console.WriteLine(i);
+            #endregion
+
+            #region Proplem11
+            //int baseNumber, powrNmber , value = 1;
+            //Console.Write("Enter the base number : ");
+            //int.TryParse(Console.ReadLine(), out baseNumber);
+            //Console.Write("Enter the powr number : ");
+            //int.TryParse(Console.ReadLine(), out powrNmber);
+            //while (powrNmber > 0)
+            //{
+            //    value *= baseNumber;
+            //    powrNmber--;
+            //}
+            //Console.WriteLine(value);
+            #endregion
+
+            #region Proplem12
+            //int[] marks = new int[5];
+            //int total = 0;
+            //double average , percentage;
+            //for (int i = 0; i < 5; i++) 
+            //{
+            //    Console.Write("Enter the subjects " + (i+1).ToString() + " : " ) ;
+            //    int.TryParse(Console.ReadLine(), out marks[i]) ;
+            //}
+            //foreach (int i in marks) 
+            //{
+            //    total += i;
+            //}
+            //average = (double)total / 5;
+            //percentage = ((double)total / 500) * 100;
+            //Console.WriteLine("The Toeal is : " + total.ToString());
+            //Console.WriteLine("The Avarage is : " + average.ToString());
+            //Console.WriteLine("The Percentage is : " + percentage.ToString());
+            #endregion
+
+//Finshed  it 
+
+        }
+    }
+}
